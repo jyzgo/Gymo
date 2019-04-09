c9072684b9372c38cb2056d772d36d7bd74013a7
jyzHome <jyzgo@163.com>
2019-04-09 23:35:59 +1000

    first commit

---
+++ lib/NewPlan.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewPlanRoute extends CupertinoPageRoute<NewPlanRoute> {
+  NewPlanRoute() : super(builder: (BuildContext context) => new NewPlanPage());
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return SlideTransition(
+        position: new Tween<Offset>(
+          begin: Offset.zero,
+          end: const Offset(-1.0, 0.0),
+        ).animate(secondaryAnimation),
+        child: NewPlanPage());
+  }
+}
+
+class NewPlanPage extends StatefulWidget {
+  final Widget child;
+
+  NewPlanPage({Key key, this.child}) : super(key: key);
+
+  _NewPlanPageState createState() => _NewPlanPageState();
+}
+
+class _NewPlanPageState extends State<NewPlanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('New Plan'),
+        ),
+        body: Center(
+          child: Text('New plan'),
+        ));
+  }
+}
