70076c0645a45330dd0f3656110c6b23a6d8b664
EthanJiang <jyzgo0125@gmail.com>
2019-05-21 14:17:27 +1000

    move homepage to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import './UI/NewPlan.dart';
-import './utils/FileManager.dart';
+import './UI/GymoHomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,94 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'GymoHome'),
+      home: GymoHomePage(title: 'GymoHome'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const String JSON_FILE = 'gymo.json';
-  static const String ARRAY_KEY = 'arrays';
-  static const String TITLE_KEY = 'title';
-  List<Map<String, dynamic>> _plans;
-  _MyHomePageState() {
-    _loadFile();
-  }
-
-  _loadFile() async {
-    var file = File(JSON_FILE);
-    if (await file.exists()) {
-      FileManager().readJson(JSON_FILE).then((mainMap) => {
-            setState(() {
-              if (mainMap[ARRAY_KEY] == null) {
-                mainMap[ARRAY_KEY] = List<Map<String, dynamic>>();
-              }
-              _plans = mainMap[ARRAY_KEY];
-            })
-          });
-    } else {
-      _plans = List<Map<String, dynamic>>();
-    }
-  }
-
-  List<Widget> _cellList() {
-    if (_plans != null) {
-      return _plans
-          .map((plan) => ListTile(title: Text(plan[TITLE_KEY])))
-          .toList();
-    }
-    return null;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.settings),
-              tooltip: 'Open setting',
-              onPressed: () {
-                print('Setting pressed');
-              })
-        ],
-      ),
-      body: Center(
-          child: _plans != null && _plans.length != 0
-              ? ListView(
-                  children: _cellList(),
-                )
-              : Text('Add A New Plan',
-                  style: TextStyle(fontSize: 40, color: Colors.black))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => NewPlanPage()))
-              .then((v) => {_dealWithReturnValue(v)});
-
-          // NewPlanRoute())
-          //     .then((value) => {print('value is $value')});
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  void _dealWithReturnValue(v) {
-    if (v != null) {
-      setState(() {
-        _plans.add(v);
-      });
-    }
   }
 }
